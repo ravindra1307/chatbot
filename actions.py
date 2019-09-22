a74329903ec52afef68919225ee207c938880ab8
Ravindra sharma <sha.ravindra1307@gmail.com>
2019-09-23 04:19:37 +0530

    Add files via upload

--- actions.py
+++ actions.py
@@ -1,11 +1,110 @@
-from typing import Any, Text, Dict, List
+from typing import Any, Text, Dict, List, Union, Optional
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet, Restarted, FollowupAction
-
+from rasa_sdk.forms import FormAction
 import sqlite3
 
 offset_value = 0
+
+
+
+class JobForm(FormAction):
+    """Example of a custom form action"""
+
+    def name(self) -> Text:
+        """Unique identifier of the form"""
+
+        return "job_form"
+
+    @staticmethod
+    def required_slots(tracker: Tracker) -> List[Text]:
+        """A list of required slots that the form has to fill"""
+
+        return ["skills", "experience", "location"]
+
+    def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict]]]:
+        """A dictionary to map required slots to
+            - an extracted entity
+            - intent: value pairs
+            - a whole message
+            or a list of them, where a first match will be picked"""
+            
+        #"loan_amount": self.from_text(intent=None),
+    
+
+        return {
+            "skills": [
+                self.from_text(intent=["my_skills","skills_job",None]),
+                #self.from_entity(
+                #    entity="skills",intent=["my_skills","skills_job",None]),
+            ],
+            "experience": [
+                self.from_entity(
+                    entity="experience", intent=["experience"]
+                ),
+            ],
+            "location": [
+                self.from_text(intent=["location",None]),
+                #self.from_entity(entity="location", intent=["location",None]),
+            ],
+        }
+
+    
+    # USED FOR DOCS: do not rename without updating in docs
+    def validate_skills(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Optional[Text]:
+        """Validate cuisine value."""
+
+        
+        return {"skills": value}
+      
+
+    def validate_experience(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Optional[Text]:
+        """Validate num_people value."""
+
+        
+        return {"experience": value}
+        
+
+    def validate_location(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Any:
+        """Validate outdoor_seating value."""
+
+        
+        return {"location": value}
+        
+   
+    def submit(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict]:
+        """Define what the form has to do
+            after all required slots are filled"""
+
+        # utter submit template
+        #dispatcher.utter_template("utter_application", tracker)
+        return []
+
+
 
 class action_search_jobs(Action):
     def name(self) -> Text:
@@ -13,11 +112,27 @@ class action_search_jobs(Action):
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain):
     
     
+        #dispatcher.utter_template("utter_application", tracker)
         global offset_value
             
         offset_value = 0
         conn = sqlite3.connect('job_data.db')     
         result = tracker.get_slot('skills')
+        experience_req = tracker.get_slot('experience')
+        location_pref = tracker.get_slot('location')
+        
+        if experience_req == '0':
+            experience_format = 0
+        elif experience_req == '0-2':
+            experience_format = 2
+        elif experience_req == '2-4':
+            experience_format = 4
+        else:
+            experience_format = 25
+        
+        
+        
+        
         
         if result == None:
             result=[]
@@ -28,7 +143,8 @@ class action_search_jobs(Action):
             split_all = split_comma+split_space
             for i in skills_list:
                 if i in split_all:
-                    result.append(i)   
+                    result.append(i)
+                    
                     
             
             if len(result)>2:       
@@ -36,6 +152,7 @@ class action_search_jobs(Action):
                           WHERE skills_req like '%{}%' or  skills_req like '%{}%' or  skills_req like '%{}%' LIMIT 5\
                           ".format(result[0].lower(),result[1].lower(),result[2].lower()))
             elif len(result)==2:
+                #dispatcher.utter_message("s")
                 q = conn.execute("SELECT DISTINCT company,position from joblistings \
                           WHERE skills_req like '%{}%' or  skills_req like '%{}%' LIMIT 5 \
                           ".format(result[0].lower(),result[1].lower()))
@@ -82,16 +199,19 @@ class action_search_jobs(Action):
         
             if len(result)>2:       
                     q = conn.execute("SELECT DISTINCT company,position from joblistings \
-                              WHERE skills_req like '%{}%' or  skills_req like '%{}%' or  skills_req like '%{}%' LIMIT 5\
-                              ".format(result[0].lower(),result[1].lower(),result[2].lower()))
+                              WHERE (skills_req like '%{}%' or  skills_req like '%{}%' or  skills_req like '%{}%')\
+                              and exp_req <= '{}' and job_location = '{}' LIMIT 5\
+                              ".format(result[0].lower(),result[1].lower(),result[2].lower(),experience_format,location_pref.lower()))
             elif len(result)==2:
+                    dispatcher.utter_message("select the position for which you want to enquire")
                     q = conn.execute("SELECT DISTINCT company,position from joblistings \
-                              WHERE skills_req like '%{}%' or  skills_req like '%{}%' LIMIT 5 \
-                              ".format(result[0].lower(),result[1].lower()))
+                              WHERE (skills_req like '%{}%' or  skills_req like '%{}%')\
+                              and exp_req <= '{}' and job_location = '{}' LIMIT 5 \
+                              ".format(result[0].lower(),result[1].lower(),experience_format,location_pref.lower()))
             else:
                 q = conn.execute("SELECT DISTINCT company,position from joblistings \
-                              WHERE skills_req like '%{}%' LIMIT 5 \
-                              ".format(result[0].lower()))
+                              WHERE skills_req like '%{}%' and exp_req <= '{}' and job_location = '{}' LIMIT 5 \
+                              ".format(result[0].lower(),experience_format,location_pref.lower()))
 
 
             r=(q.fetchall())
@@ -125,6 +245,18 @@ class action_search_jobs_more(Action):
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain):
         conn = sqlite3.connect('job_data.db')     
         result = tracker.get_slot('skills')
+        experience_req = tracker.get_slot('experience')
+        location_pref = tracker.get_slot('location')
+        
+        if experience_req == '0':
+            experience_format = 0
+        elif experience_req == '0-2':
+            experience_format = 2
+        elif experience_req == '2-4':
+            experience_format = 4
+        else:
+            experience_format = 25
+        
         result = result.replace(' ',',')
         
         if ',' in result:
@@ -139,6 +271,24 @@ class action_search_jobs_more(Action):
         offset_value += 5
         
         if len(result)>2:       
+                    q = conn.execute("SELECT DISTINCT company,position from joblistings \
+                              WHERE (skills_req like '%{}%' or  skills_req like '%{}%' or  skills_req like '%{}%')\
+                              and exp_req <= '{}' and job_location = '{}' LIMIT 5  OFFSET {}\
+                              ".format(result[0].lower(),result[1].lower(),result[2].lower(),experience_format,location_pref.lower(),offset_value))
+        elif len(result)==2:
+                    #dispatcher.utter_message("select the position for which you want to enquire")
+                    q = conn.execute("SELECT DISTINCT company,position from joblistings \
+                              WHERE (skills_req like '%{}%' or  skills_req like '%{}%')\
+                              and exp_req <= '{}' and job_location = '{}' LIMIT 5  OFFSET {}\
+                              ".format(result[0].lower(),result[1].lower(),experience_format,location_pref.lower(),offset_value))
+        else:
+                q = conn.execute("SELECT DISTINCT company,position from joblistings \
+                              WHERE skills_req like '%{}%' and exp_req <= '{}' and job_location = '{}' LIMIT 5  OFFSET {}\
+                              ".format(result[0].lower(),experience_format,location_pref.lower(),offset_value))
+            
+       
+        '''
+        if len(result)>2:       
                 q = conn.execute("SELECT DISTINCT company,position from joblistings \
                           WHERE skills_req like '%{}%' or  skills_req like '%{}%' or  skills_req like '%{}%' LIMIT 5 OFFSET {}\
                           ".format(result[0].lower(),result[1].lower(),result[2].lower(),offset_value))
@@ -150,7 +300,7 @@ class action_search_jobs_more(Action):
             q = conn.execute("SELECT DISTINCT company,position from joblistings \
                           WHERE skills_req like '%{}%' LIMIT 5 OFFSET {}\
                           ".format(result[0].lower(),offset_value))
-
+        '''
 
         r=(q.fetchall())
         dispatcher.utter_message("select the position for which you want to enquire")
@@ -180,6 +330,8 @@ class action_search_job_desc(Action):
           x=row[0]
     
       dispatcher.utter_message(x)
+      dispatcher.utter_template('utter_ask_job_queries',tracker)
+      #return [FollowupAction("action_search_jobs")]
 
 class action_greet(Action):
     def name(self) -> Text:
@@ -217,7 +369,8 @@ class action_greet(Action):
         dispatcher.utter_template('utter_out_of_context',tracker)
       else:
         dispatcher.utter_template('utter_greet',tracker)
-        
+        dispatcher.utter_template('utter_start',tracker)
+        #return [Restarted()]
      # return [FollowupAction("action_search_jobs")]
       
 
@@ -251,6 +404,7 @@ class action_search_exp(Action):
           x=row[0]
     
       dispatcher.utter_message(x+' years')
+      dispatcher.utter_template('utter_ask_job_queries',tracker)
 
 
 
@@ -268,6 +422,7 @@ class action_search_jobtime(Action):
           x=row[0]
     
       dispatcher.utter_message('timing is :' + x)
+      dispatcher.utter_template('utter_ask_job_queries',tracker)
 
 
 
@@ -286,6 +441,7 @@ class action_search_salary(Action):
           x=row[0]
     
       dispatcher.utter_message(x)
+      dispatcher.utter_template('utter_ask_job_queries',tracker)
 
 class action_search_address(Action):
     def name(self) -> Text:
@@ -331,6 +487,7 @@ class action_eligibility_criteria(Action):
           x=row[0]
       
       dispatcher.utter_message("qualifications required are " + x)
+      dispatcher.utter_template('utter_ask_job_queries',tracker)
        
         
         
